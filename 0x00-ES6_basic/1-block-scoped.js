03361d27685cf3e4ff3eb0d827361251a2482bda
Enioluwayan7 <olabanjienioluwayan@gmail.com>
2024-10-01 21:46:23 +0000

    update

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,9 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;   // Use const for variables that don't change
+  const task = false; // Use const for variables that don't change
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;  // Use const or let to create block-scoped variables
+    const task = true; // Use const or let to create block-scoped variables
     const task2 = false;
   }
 
