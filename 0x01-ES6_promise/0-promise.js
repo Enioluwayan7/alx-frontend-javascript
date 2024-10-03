8b9a4012bb58483c3eca9c62dca36931f93b057b
Enioluwayan7 <olabanjienioluwayan@gmail.com>
2024-10-03 21:32:04 +0000

    add

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,12 +1,3 @@
-function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // Simulate an API call or async operation
-    const success = true; // change this to false to simulate a failure
-
-    if (success) {
-      resolve("Success: Data retrieved from API.");
-    } else {
-      reject("Error: Failed to retrieve data from API.");
-    }
-  });
+export default function getResponseFromAPI() {
+  return Promise.resolve(true);
 }
