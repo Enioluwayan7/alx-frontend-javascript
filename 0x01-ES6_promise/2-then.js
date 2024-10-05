7d125b7e34e3fc7b6b76da0ca4e7a69ac10b4c5e
Enioluwayan7 <olabanjienioluwayan@gmail.com>
2024-10-05 08:49:19 +0000

    add

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -8,20 +8,6 @@ function handleResponseFromAPI(promise) {
       };
     })
     .catch(() => {
-      console.log('Got a response from the API');
       return new Error();
     });
 }
-
-// Test with a sample promise
-const samplePromise = new Promise((resolve, reject) => {
-  // Simulating success or failure
-  const success = true; // Change this to `false` to simulate a failure
-  if (success) {
-    resolve();
-  } else {
-    reject();
-  }
-});
-
-handleResponseFromAPI(samplePromise).then((result) => console.log(result));
